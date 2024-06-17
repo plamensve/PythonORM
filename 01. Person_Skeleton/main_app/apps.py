f277b90843ff3ee3dc435e3984f9350f9881ad2b
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-06-17 13:21:50 +0300

    17.06.2024 - delete test.py file

--- 01. Person_Skeleton/main_app/apps.py
+++ 01. Person_Skeleton/main_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TasksConfig(AppConfig):
+class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
+    name = 'main_app'
