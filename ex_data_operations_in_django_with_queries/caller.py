f6efd0130438e01dc95404617c38f7542b0b7465
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-06-26 21:54:34 +0300

    Update

--- ex_data_operations_in_django_with_queries/caller.py
+++ ex_data_operations_in_django_with_queries/caller.py
@@ -5,6 +5,23 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from django.db import models
+from main_app.models import Pet
 
-# Create queries within functions
+
+def create_pet(name: str, species: str):
+    pet = Pet(
+        name=name,
+        species=species
+    )
+    pet.save()
+    result = []
+
+    for p in Pet.objects.all():
+        result.append(f"{p.name} is a very cute {p.species}!")
+
+    return '\n'.join(result)
+
+# print(create_pet('Buddy', 'Dog'))
+# print(create_pet('Whiskers', 'Cat'))
+# print(create_pet('Rocky', 'Hamster'))
