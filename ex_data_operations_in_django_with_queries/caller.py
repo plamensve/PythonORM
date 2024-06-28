1ee24e93596215056f0f046cab3b67f017a4356d
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-06-28 20:14:09 +0300

    Update - 28.06.2024

--- ex_data_operations_in_django_with_queries/caller.py
+++ ex_data_operations_in_django_with_queries/caller.py
@@ -1,12 +1,13 @@
 import os
 import django
+from unicodedata import decimal
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 from django.db import models
-from main_app.models import Pet, Artifact, Location
+from main_app.models import Pet, Artifact, Location, Car, Task, HotelRoom
 
 
 def create_pet(name: str, species: str):
@@ -64,6 +65,7 @@ def show_all_locations():
     result = []
     for i in Location.objects.all().order_by('-id'):
         result.append(f"{i.name} has a population of {i.population}!")
+
     return '\n'.join(result)
 
 
@@ -89,3 +91,103 @@ def delete_first_location():
 # print(get_capitals())
 
 
+def apply_discount():
+    cars = Car.objects.all()
+
+    # for c in car:
+    #     discount = 0
+    #     for n in str(c.year):
+    #         discount += int(n)
+    #     discounted_price = float(c.price) - (discount / 100) * float(c.price)
+    #     c.price_with_discount = discounted_price
+    #     c.save()
+
+    for c in cars:
+        discount_off = sum(int(d) for d in str(c.year)) / 100
+        money_discount = float(c.price) * discount_off
+        c.price_with_discount = float(c.price) - money_discount
+        c.save()
+
+
+# apply_discount()
+
+def get_recent_cars():
+    return Car.objects.filter(year__gt=2020).values('model', 'price_with_discount')
+
+
+def delete_last_car():
+    Car.objects.last().delete()
+
+
+def show_unfinished_tasks():
+    task = Task.objects.all().filter(is_finished=False)
+    return '\n'.join(str(t) for t in task)
+
+
+# print(show_unfinished_tasks())
+
+def complete_odd_tasks():
+    task = Task.objects.all()
+    for t in task:
+        if t.id % 2 == 1:
+            t.is_finished = True
+            t.save()
+
+
+# complete_odd_tasks()
+
+def encode_and_replace(text: str, task_title: str):
+    decoded_text = ''.join(chr(ord(symbol) - 3) for symbol in text)
+    Task.objects.filter(title=task_title).update(description=decoded_text)
+
+    # task = Task.objects.all()
+    #
+    # encoded = text
+    # decoded = [chr(ord(i) - 3) for i in encoded]
+    # for t in task:
+    #     if t.title == task_title:
+    #         t.description = ''.join(decoded)
+    #     t.save()
+
+
+# encode_and_replace("Zdvk#wkh#glvkhv$", "Simple Task")
+# print(Task.objects.get(title='Simple Task').description)
+
+
+def get_deluxe_rooms():
+    rooms = HotelRoom.objects.all().filter(room_type='Deluxe')
+    even_deluxe_rooms = [str(r) for r in rooms if r.id % 2 == 0]
+
+    return "\n".join(even_deluxe_rooms)
+
+
+# print(get_deluxe_rooms())
+
+def increase_room_capacity():
+    rooms = HotelRoom.objects.all().order_by('id')
+
+    previous_room_capacity = None
+
+    for room in rooms:
+        if not room.is_reserved:
+            continue
+
+        if previous_room_capacity is not None:
+            room.capacity += previous_room_capacity
+        else:
+            room.capacity = room.capacity + room.id
+
+        previous_room_capacity = room.capacity
+        room.save()
+
+
+def reserve_first_room():
+    room = HotelRoom.objects.first()
+    room.is_reserved = True
+    room.save()
+
+
+def delete_last_room():
+    room = HotelRoom.objects.last()
+    if not room.is_reserved:
+        room.delete()
--- ex_data_operations_in_django_with_queries/main_app/models.py
+++ ex_data_operations_in_django_with_queries/main_app/models.py
@@ -22,4 +22,72 @@ class Location(models.Model):
     is_capital = models.BooleanField(default=False)
 
 
+class Car(models.Model):
+    model = models.CharField(max_length=40)
+    year = models.PositiveIntegerField()
+    color = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_with_discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Task - {self.title} needs to be done until {self.due_date}!"
+
+
+class RoomChoices(models.TextChoices):
+    STANDARD = 'S', 'Standard'
+    DELUXE = 'D', 'Deluxe'
+    SUITE = 'SU', 'Suite'
+
+
+class HotelRoom(models.Model):
+    room_number = models.PositiveIntegerField()
+    room_type = models.CharField(max_length=10, choices=RoomChoices.choices)
+    capacity = models.PositiveIntegerField()
+    amenities = models.TextField()
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)
+    is_reserved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Deluxe room with number {self.room_number} costs {self.price_per_night}$ per night!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
