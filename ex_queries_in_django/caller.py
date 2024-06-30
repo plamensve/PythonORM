d298b882e43497411c1506644939f06e9f6fb31e
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-01 00:50:13 +0300

    Update - 30.06.2024

--- ex_queries_in_django/caller.py
+++ ex_queries_in_django/caller.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import ArtworkGallery
+from main_app.models import ArtworkGallery, Laptop
 
 
 def show_highest_rated_art():
@@ -40,10 +40,99 @@ def delete_negative_rated_arts():
     arts.delete()
 
 
-artwork1 = ArtworkGallery(artist_name='Vincent van Gogh', art_name='Starry Night', rating=4, price=1200000.0)
-artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa', rating=5, price=1500000.0)
+# artwork1 = ArtworkGallery(artist_name='Vincent van Gogh', art_name='Starry Night', rating=4, price=1200000.0)
+# artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa', rating=5, price=1500000.0)
+
 
 # # Bulk saves the instances
 # bulk_create_arts(artwork1, artwork2)
 # print(show_highest_rated_art())
 # print(ArtworkGallery.objects.all())
+
+
+def show_the_most_expensive_laptop():
+    laptop = Laptop.objects.order_by('-price', '-id').first()
+    return f"{laptop.brand} is the most expensive laptop available for {laptop.price}$!"
+
+
+def bulk_create_laptops(*args):
+    Laptop.objects.bulk_create(*args)
+
+
+def update_to_512_GB_storage():
+    Laptop.objects.filter(brand__in=['Asus', 'Lenovo']).update(storage=512)
+
+
+def update_to_16_GB_memory():
+    Laptop.objects.filter(brand__in=['Apple', 'Dell', 'Acer']).update(memory=16)
+
+
+def update_operation_systems():
+    Laptop.objects.filter(brand__exact='Apple').update(operation_system='MacOS')
+    Laptop.objects.filter(brand__exact='Asus').update(operation_system='Windows')
+    Laptop.objects.filter(brand__exact='Lenovo').update(operation_system='Chrome OS')
+    Laptop.objects.filter(brand__in=['Dell', 'Acer']).update(operation_system='Linux')
+
+
+def delete_inexpensive_laptops():
+    Laptop.objects.filter(price__lt=1200).delete()
+
+
+
+# laptop1 = Laptop(
+#     brand='Asus',
+#     processor='Intel Core i5',
+#     memory=8,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=899.99
+# )
+# laptop2 = Laptop(
+#     brand='Apple',
+#     processor='Chrome OS',
+#     memory=16,
+#     storage=256,
+#     operation_system='MacOS',
+#     price=1399.99
+# )
+# laptop3 = Laptop(
+#     brand='Lenovo',
+#     processor='AMD Ryzen 7',
+#     memory=12,
+#     storage=256,
+#     operation_system='Linux',
+#     price=999.99,
+# )
+
+# # Create a list of instances
+# laptops_to_create = [laptop1, laptop2, laptop3]
+#
+# # Use bulk_create to save the instances
+# bulk_create_laptops(laptops_to_create)
+#
+# update_to_512_GB_storage()
+# update_operation_systems()
+#
+# # Retrieve 2 laptops from the database
+# asus_laptop = Laptop.objects.filter(brand__exact='Asus').get()
+# lenovo_laptop = Laptop.objects.filter(brand__exact='Lenovo').get()
+#
+# print(asus_laptop.storage)
+# print(lenovo_laptop.operation_system)
+#
+# print(show_the_most_expensive_laptop())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ex_queries_in_django/main_app/models.py
+++ ex_queries_in_django/main_app/models.py
@@ -69,36 +69,25 @@ class ArtworkGallery(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
+class Laptop(models.Model):
+    LAPTOPS = (
+        ('Asus', 'Asus'),
+        ('Acer', 'Acer'),
+        ('Apple', 'Apple'),
+        ('Lenovo', 'Lenovo'),
+        ('Dell', 'Dell'),
+    )
 
+    OS = (
+        ('Windows', 'Windows'),
+        ('MacOS', 'MacOS'),
+        ('Linux', 'Linux'),
+        ('Chrome OS', 'Chrome OS'),
+    )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    brand = models.CharField(max_length=20, choices=LAPTOPS)
+    processor = models.CharField(max_length=100)
+    memory = models.PositiveIntegerField(help_text='Memory in GB')
+    storage = models.PositiveIntegerField(help_text='Storage in GB')
+    operation_system = models.CharField(max_length=20, choices=OS)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
