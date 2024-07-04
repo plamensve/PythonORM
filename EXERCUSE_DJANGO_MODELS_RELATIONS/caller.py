a4054cbd94b391d17548be552a6cf35d40338faf
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-04 22:42:39 +0300

    Update exercise

--- EXERCUSE_DJANGO_MODELS_RELATIONS/caller.py
+++ EXERCUSE_DJANGO_MODELS_RELATIONS/caller.py
@@ -5,6 +5,183 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Author, Book, Artist, Song
 
-# Create queries within functions
+
+def show_all_authors_with_their_books() -> str:
+    author_with_books = []
+
+    authors = Author.objects.all().order_by('id')
+
+    for author in authors:
+        books = Book.objects.filter(author=author)
+
+        if not books:
+            continue
+
+        titles = ', '.join(b.title for b in books)
+
+        author_with_books.append(f"{author.name} has written - {titles}!")
+
+    return '\n'.join(author_with_books)
+
+
+def delete_all_authors_without_books():
+    Author.objects.filter(book__isnull=True).delete()
+
+
+# # Create authors
+# author1 = Author.objects.create(name="J.K. Rowling")
+# author2 = Author.objects.create(name="George Orwell")
+# author3 = Author.objects.create(name="Harper Lee")
+# author4 = Author.objects.create(name="Mark Twain")
+#
+# # Create books associated with the authors
+# book1 = Book.objects.create(
+#     title="Harry Potter and the Philosopher's Stone",
+#     price=19.99,
+#     author=author1
+# )
+# book2 = Book.objects.create(
+#     title="1984",
+#     price=14.99,
+#     author=author2
+# )
+#
+# book3 = Book.objects.create(
+#     title="To Kill a Mockingbird",
+#     price=12.99,
+#     author=author3
+# )
+#
+# # Display authors and their books
+# authors_with_books = show_all_authors_with_their_books()
+# print(authors_with_books)
+
+# # Delete authors without books
+# delete_all_authors_without_books()
+# print(Author.objects.count())
+
+
+def add_song_to_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
+
+def get_songs_by_artist(artist_name: str):
+    return Artist.objects.get(name=artist_name).songs.all().order_by('-id')
+
+def remove_song_from_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.remove(song)
+
+
+
+
+# # Create artists
+# artist1 = Artist.objects.create(name="Daniel Di Angelo")
+# artist2 = Artist.objects.create(name="Indila")
+# # Create songs
+# song1 = Song.objects.create(title="Lose Face")
+# song2 = Song.objects.create(title="Tourner Dans Le Vide")
+# song3 = Song.objects.create(title="Loyalty")
+#
+# # Add a song to an artist
+# add_song_to_artist("Daniel Di Angelo", "Lose Face")
+# add_song_to_artist("Daniel Di Angelo", "Loyalty")
+# add_song_to_artist("Indila", "Tourner Dans Le Vide")
+#
+# # Get all songs by a specific artist
+# songs = get_songs_by_artist("Daniel Di Angelo")
+# for song in songs:
+#     print(f"Daniel Di Angelo: {song.title}")
+#
+# # Get all songs by a specific artist
+# songs = get_songs_by_artist("Indila")
+# for song in songs:
+#     print(f"Indila: {song.title}")
+#
+# # Remove a song from an artist
+# remove_song_from_artist("Daniel Di Angelo", "Lose Face")
+#
+# # Check if the song is removed
+# songs = get_songs_by_artist("Daniel Di Angelo")
+#
+# for song in songs:
+#     print(f"Songs by Daniel Di Angelo after removal: {song.title}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
