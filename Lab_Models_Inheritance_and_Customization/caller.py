9106b3db217eed34e9eec51d816914198bf8326e
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-07 11:38:05 +0300

    New Lab - Update

--- Lab_Models_Inheritance_and_Customization/caller.py
+++ Lab_Models_Inheritance_and_Customization/caller.py
@@ -5,6 +5,19 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+# from main_app.models import ZooKeeper, Veterinarian, Mammal
+#
+# # Keep the data from the previous exercise, so you can reuse it
+#
+# zookeeper = ZooKeeper.objects.create(first_name="Peter", last_name="Johnson", phone_number="0899524265", specialty="Mammals")
+# mammal = Mammal.objects.get(name="Fluffy")
+# zookeeper.managed_animals.add(mammal)
+# veterinarian = Veterinarian.objects.create(first_name="Dr. Michael", last_name="Smith", phone_number="9876543210", license_number="VET12345")
+#
+# zookeeper_from_db = ZooKeeper.objects.first()
+# print(f"{zookeeper_from_db.first_name} {zookeeper_from_db.last_name} is a ZooKeeper.")
+#
+# veterinarian_from_db = Veterinarian.objects.first()
+# print(f"{veterinarian_from_db.first_name} {veterinarian_from_db.last_name} is a Veterinarian.")
 
-# Create queries within functions
+
--- Lab_Models_Inheritance_and_Customization/main_app/models.py
+++ Lab_Models_Inheritance_and_Customization/main_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    sound = models.CharField(max_length=100)
+
+
+class Mammal(Animal):
+    fur_color = models.CharField(max_length=50)
+
+
+class Bird(Animal):
+    wing_span = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Reptile(Animal):
+    scale_type = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+
+class ZooKeeper(Employee):
+    class SpecialtyChoices(models.TextChoices):
+        MAMMALS = 'Mammals', 'Mammals'
+        BIRDS = 'Birds', 'Birds'
+        REPTILES = 'Reptiles', 'Reptiles'
+        OTHERS = 'Others', 'Others'
+
+    specialty = models.CharField(max_length=10, choices=SpecialtyChoices.choices)
+    managed_animals = models.ManyToManyField(Animal)
+
+class Veterinarian(Employee):
+    license_number = models.CharField(max_length=10)
