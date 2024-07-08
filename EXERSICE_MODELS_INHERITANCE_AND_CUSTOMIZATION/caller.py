d40831c9b6c83d4a3240277219d6c43975502065
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-08 21:24:03 +0300

    Exercise - 08.07.2024

--- EXERSICE_MODELS_INHERITANCE_AND_CUSTOMIZATION/caller.py
+++ EXERSICE_MODELS_INHERITANCE_AND_CUSTOMIZATION/caller.py
@@ -1,10 +1,60 @@
 import os
 import django
 
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Mage, Necromancer, UserProfile, Message
 
-# Create queries within functions
+# Create instances
+# mage = Mage.objects.create(
+#     name="Fire Mage",
+#     description="A powerful mage specializing in fire magic.",
+#     elemental_power="Fire",
+#     spellbook_type="Ancient Grimoire"
+# )
+#
+# necromancer = Necromancer.objects.create(
+#     name="Dark Necromancer",
+#     description="A mage specializing in dark necromancy.",
+#     elemental_power="Darkness", spellbook_type="Necronomicon",
+#     raise_dead_ability="Raise Undead Army"
+# )
+# print(mage.elemental_power)
+# print(mage.spellbook_type)
+# print(necromancer.name)
+# print(necromancer.description)
+# print(necromancer.raise_dead_ability)
+
+
+# user1 = UserProfile.objects.create(username='john_doe', email='john@example.com', bio='Hello, I am John Doe.')
+#
+# user2 = UserProfile.objects.create(username='jane_smith', email='jane@example.com', bio='Hi there, I am Jane Smith.')
+#
+# user3 = UserProfile.objects.create(username='alice', email='alice@example.com', bio='Hello, I am Alice.')
+#
+# # Create a message from user1 to user2
+# message1 = Message.objects.create(
+#     sender=user1,
+#     receiver=user2,
+#     content="Hello, Jane! Could you please tell Alice that tomorrow we are going on vacation?")
+#
+# # Display the content
+# print(message1.content)
+# # Mark the message as read
+# message1.mark_as_read()
+# print(f"Is read: {message1.is_read}")
+#
+# # Create a reply from user2 to user1
+# reply_message = message1.reply_to_message(
+#     reply_content="Hi John, sure! I will forward this message to her!")
+#
+# # Display the content
+# print(reply_message.content)
+#
+# # Create a forwarded message from user2 to user3
+# forwarded_message = message1.forward_message(receiver=user3)
+#
+# print(f"Forwarded message from {forwarded_message.sender.username} to {forwarded_message.receiver.username}")
--- EXERSICE_MODELS_INHERITANCE_AND_CUSTOMIZATION/main_app/models.py
+++ EXERSICE_MODELS_INHERITANCE_AND_CUSTOMIZATION/main_app/models.py
@@ -1,3 +1,105 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class BaseCharacter(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(max_length=100)
+    spellbook_type = models.CharField(max_length=100)
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    assassination_technique = models.CharField(max_length=100)
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    demon_slaying_ability = models.CharField(max_length=100)
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(max_length=100)
+    temporal_shift_ability = models.CharField(max_length=100)
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(max_length=100)
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(max_length=100)
+    venomous_bite_ability = models.CharField(max_length=100)
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(max_length=100)
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(max_length=100)
+    retribution_ability = models.CharField(max_length=100)
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserProfile, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self) -> None:
+        if not self.is_read:
+            self.is_read = True
+            self.save()
+
+    def reply_to_message(self, reply_content: str):
+        message = Message(
+            sender=self.receiver,
+            receiver=self.sender,
+            content=reply_content,
+        )
+        message.save()
+        return message
+
+    def forward_message(self, receiver: UserProfile):
+        message = Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=self.content,
+        )
+        message.save()
+        return message
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
