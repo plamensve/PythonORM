85f6f89d5ec08d9e5aa27627b31cbaee09d3006d
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-10 21:25:06 +0300

    update

--- Advance_django_techniques/caller.py
+++ Advance_django_techniques/caller.py
@@ -1,10 +1,9 @@
 import os
 import django
 
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
 
-# Create queries within functions
