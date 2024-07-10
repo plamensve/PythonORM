f1fa475d9861214b369b21c3b23cf3d23435f16d
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-10 22:22:31 +0300

    update

--- Advance_django_techniques/main_app/admin.py
+++ Advance_django_techniques/main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main_app.models import RestaurantReview
+
 
 # Register your models here.
+@admin.register(RestaurantReview)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
--- Advance_django_techniques/main_app/models.py
+++ Advance_django_techniques/main_app/models.py
@@ -36,3 +36,42 @@ class Menu(models.Model):
         validators=[validate_menu_categories]
     )
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+
+class RestaurantReview(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
+
+    class Meta:
+        ordering = ('-rating',)
+        verbose_name = 'Restaurant Review'
+        verbose_name_plural = 'Restaurant Reviews'
+        unique_together = ['reviewer_name', 'restaurant']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
