acb573a94892898cc5ecab533828cbfb4c3aee44
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-13 21:27:54 +0300

    EX_QUERIES

--- EX_QUERIES/main_app/models.py
+++ EX_QUERIES/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField(Song, related_name='artists')
