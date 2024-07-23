baeecd634f80a6d94ebd3a66b8e1d8ff9592e6f3
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-23 11:45:18 +0300

    Exam prep 7

--- EXAM_PREP_7/caller.py
+++ EXAM_PREP_7/caller.py
@@ -1,10 +1,58 @@
 import os
 import django
 
-# Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import TennisPlayer
+from django.db.models import Q, Count
 
-# Create queries within functions
+
+def get_tennis_players(search_name=None, search_country=None):
+    global query
+
+    if search_name is None and search_country is None:
+        return ''
+
+    if search_name and search_country:
+        query = Q(full_name__icontains=search_name) & Q(country__icontains=search_country)
+
+    elif search_name and search_country is None:
+        query = Q(full_name__icontains=search_name)
+
+    elif search_name is None and search_country:
+        query = Q(country__icontains=search_country)
+
+    players = TennisPlayer.objects.filter(query).order_by('ranking')
+
+    if not players.exists():
+        return ''
+
+    result = []
+    for p in players:
+        result.append(f"Tennis Player: {p.full_name}, country: {p.country}, ranking: {p.ranking}")
+
+    return '\n'.join(result)
+
+
+def get_top_tennis_player():
+    player = TennisPlayer.objects.get_tennis_players_by_wins_count().first()
+
+    if not player:
+        return ''
+
+    return f"Top Tennis Player: {player.full_name} with {player.number_of_wins} wins."
+
+
+def get_tennis_player_by_matches_count():
+    player = ((TennisPlayer.objects.annotate(matches_count=Count('match_players'))
+               .order_by('-matches_count', 'ranking'))
+              .first())
+
+    if not player or not player.matches_count:
+        return ''
+
+    return f"Tennis Player: {player.full_name} with {player.matches_count} matches played."
+
+
+print(get_tennis_player_by_matches_count())
--- EXAM_PREP_7/main_app/models.py
+++ EXAM_PREP_7/main_app/models.py
@@ -1,3 +1,42 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from main_app.manager import TennisPlayerManager
+
+
+class TennisPlayer(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(5)])
+    birth_date = models.DateField()
+    country = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    ranking = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(300)])
+    is_active = models.BooleanField(default=True)
+
+    objects = TennisPlayerManager()
+
+    def __str__(self):
+        return self.full_name
+
+class Tournament(models.Model):
+    class SurfaceTypeChoices(models.TextChoices):
+        NOT_SELECTED = 'Not Selected', 'Not Selected'
+        CLAY = 'Clay', 'Clay'
+        GRASS = 'Grass', 'Grass'
+        HARD_COURT = 'Hard Court', 'Hard Court'
+
+    name = models.CharField(max_length=150, validators=[MinLengthValidator(2)], unique=True)
+    location = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    prize_money = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    surface_type = models.CharField(max_length=12, default='Not Selected', choices=SurfaceTypeChoices.choices)
+
+
+class Match(models.Model):
+    score = models.CharField(max_length=100)
+    summary = models.TextField(validators=[MinLengthValidator(5)])
+    date_played = models.DateTimeField()
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='matches')
+    players = models.ManyToManyField(TennisPlayer, 'match_players')
+    winner = models.ForeignKey(TennisPlayer, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Matches"
