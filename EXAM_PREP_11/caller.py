093d3012eebd6b1499314739ead8ef2789717ad8
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-07-28 14:33:13 +0300

    Exam Prep 11

--- EXAM_PREP_11/caller.py
+++ EXAM_PREP_11/caller.py
@@ -56,4 +56,64 @@ def get_tennis_player_by_matches_count():
     return f"Tennis Player: {player.full_name} with {player.num_of_matches} matches played."
 
 
-print(get_tennis_player_by_matches_count())
+def get_tournaments_by_surface_type(surface=None):
+    if surface is None:
+        return ''
+
+    tournaments = ((Tournament.objects.filter(surface_type__icontains=surface)
+                    .annotate(num_of_matches=Count('match')))
+                   .order_by('-start_date'))
+
+    tours = Tournament.objects.all()
+
+    if not tournaments.exists() or not tours.exists():
+        return ''
+
+    result = []
+    for t in tournaments:
+        result.append(f"Tournament: {t.name}, start date: {t.start_date}, matches: {t.num_of_matches}")
+
+    return '\n'.join(result)
+
+
+def get_latest_match_info():
+    matches = Match.objects.all().select_related('tournament').order_by('-date_played', '-id')
+    match = matches.first()
+
+    if not match:
+        return ''
+
+    players_in_match = []
+    for p in match.players.all().order_by('full_name'):
+        players_in_match.append(p.full_name)
+    match_winner = match.winner
+
+    result = (f"Latest match played on: {match.date_played}, "
+              f"tournament: {match.tournament.name}, "
+              f"score: {match.score}, "
+              f"players: {players_in_match[0]} vs {players_in_match[1]}, "
+              f"winner: {match_winner if match_winner else 'TBA'}, "
+              f"summary: {match.summary}")
+
+    return result
+
+
+def get_matches_by_tournament(tournament_name=None):
+    if tournament_name is None:
+        return "No matches found."
+
+    matches = (Match.objects.select_related('tournament')
+               .filter(tournament__name__exact=tournament_name)).order_by('-date_played')
+
+    if not matches.exists():
+        return "No matches found."
+
+    result = []
+    for match in matches:
+        result.append(f"Match played on: {match.date_played}, "
+                      f"score: {match.score}, "
+                      f"winner: {match.winner.full_name if match.winner else 'TBA'}")
+
+    return '\n'.join(result)
+
+
