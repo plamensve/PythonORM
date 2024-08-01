9221ba4094bcc670ddd2072c02fe7a9de524603c
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-08-01 23:38:04 +0300

    Exam prep 15

---
+++ EXAM_PREP_15/main_app/mixins.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class CreationMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    creation_date = models.DateTimeField(
+        auto_now_add=True
+    )
--- EXAM_PREP_15/main_app/models.py
+++ EXAM_PREP_15/main_app/models.py
@@ -1,3 +1,70 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from main_app.mixins import CreationMixin
+
+
+class Profile(CreationMixin):
+    full_name = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(2)
+        ]
+    )
+
+    email = models.EmailField()
+
+    phone_number = models.CharField(
+        max_length=15
+    )
+
+    address = models.TextField()
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+
+class Product(CreationMixin):
+    name = models.CharField(
+        max_length=100
+    )
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.01)
+        ]
+    )
+
+    in_stock = models.PositiveIntegerField()
+
+    is_available = models.BooleanField(
+        default=True
+    )
+
+
+class Order(CreationMixin):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    products = models.ManyToManyField(
+        Product
+    )
+
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.01)
+        ]
+    )
+
+    is_completed = models.BooleanField(
+        default=False
+    )
