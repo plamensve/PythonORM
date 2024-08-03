7dd1a1e97ca9f49bbc9b0f60e39c30d7598f107b
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-08-03 08:37:59 +0300

    Exam prep 15

--- EXAM_PREP_15/caller.py
+++ EXAM_PREP_15/caller.py
@@ -1,7 +1,6 @@
 import os
 import django
 
-
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
@@ -18,7 +17,7 @@ def get_profiles(search_string=None):
         Q(full_name__icontains=search_string) |
         Q(email__icontains=search_string) |
         Q(phone_number__icontains=search_string)).annotate(nums_of_order=Count('order'))
-               .order_by('full_name'))
+                .order_by('full_name'))
 
     if not profiles:
         return ''
@@ -31,4 +30,32 @@ def get_profiles(search_string=None):
                       f"orders: {profile.nums_of_order}")
 
     return '\n'.join(result)
+
+
+def get_loyal_profiles():
+    profiles = Profile.objects.get_regular_customers()
+
+    if not profiles:
+        return ''
+
+    result = []
+
+    for profile in profiles:
+        result.append(f"Profile: {profile.full_name}, orders: {profile.number_of_orders}")
+
+    return '\n'.join(result)
+
+
+def get_last_sold_products():
+    try:
+        order = Order.objects.all().latest('creation_date')
+    except Order.DoesNotExist:
+        return ''
+
+    products = order.products.all().order_by('name')
+    if not products.exists():
+        return ''
+
+    product_names = [p.name for p in products]
+    return f"Last sold products: {', '.join(product_names)}"
 
